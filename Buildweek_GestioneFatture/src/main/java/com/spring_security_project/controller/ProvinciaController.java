bf1f162dc3985dc0a51fea2df12bb1c7d5d67df6
Antonio D'Amico <andam@hotmail.it>
2023-05-03 17:55:29 +0200

    Update ProvinciaController.java

--- Buildweek_GestioneFatture/src/main/java/com/spring_security_project/controller/ProvinciaController.java
+++ Buildweek_GestioneFatture/src/main/java/com/spring_security_project/controller/ProvinciaController.java
@@ -4,13 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.spring_security_project.service.ProvinciaService;
-@CrossOrigin(origins = "*", maxAge = 3600)
+
 @RestController
 @RequestMapping("/api/provincia")
 public class ProvinciaController {
